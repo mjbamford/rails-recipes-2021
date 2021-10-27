0dad98b2e25ba0e16c3d38e1f28005c25f89d81d
Michael Bamford <michael@michaelbamford.com>
2021-10-27 12:22:22 +1100

    Add ingredients to recipe form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,10 +3,6 @@ class RecipesController < ApplicationController
         @recipes = Recipe.all
     end
 
-    def new
-        @recipe = Recipe.new
-    end
-
     def show
         @recipe = Recipe.find params[:id]
 
@@ -17,11 +13,15 @@ class RecipesController < ApplicationController
         end
     end
 
+    def new
+        @recipe = Recipe.new
+    end
+
     def create
-        author = Author.find params[:recipe][:author_id]
-        @recipe = author.recipes.build permitted_params
+        # author = Author.find params[:recipe][:author_id]
+        # @recipe = author.recipes.build permitted_params
         # Or permit author_id in the mass assignment for the receipe.
-        # @recipe = Recipe.new permitted_params
+        @recipe = Recipe.new permitted_params
 
         respond_to do |format|
             if @recipe.save
@@ -32,7 +32,23 @@ class RecipesController < ApplicationController
         end
     end
 
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def update
+        @recipe = Recipe.find(params[:id])
+
+        respond_to do |format|
+            if @recipe.update(permitted_params)
+                format.html { redirect_to @recipe }
+            else
+                format.html { render :edit }
+            end
+        end
+    end
+
     def permitted_params
-        params.required(:recipe).permit(:name, :difficulty)
+        params.required(:recipe).permit(:name, :difficulty, :author_id, ingredient_ids: [])
     end
 end
