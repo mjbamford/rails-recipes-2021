0990a846afefaaa45512465fe6181f7ba29480cd
Michael Bamford <michael@michaelbamford.com>
2021-11-02 11:52:54 +1100

    Add view, request specs to recipes

--- spec/views/recipes/index.html.erb_spec.rb
+++ spec/views/recipes/index.html.erb_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe "recipes/index", type: :view do
   let(:author) { Author.create! name: 'Adam Ant' }
 
-  xit "renders a list of recipes" do
+  before(:each) do
     assign(:recipes, [
-      author.recipes.create!(name: 'Cake', difficulty: 3)
+      author.recipes.create!(name: 'Cake', difficulty: 3),
+      author.recipes.create!(name: 'Toast', difficulty: 1)
     ])
+  end
+
+  it "renders a list of recipes" do
+    render
+    # raise rendered.inspect
+    expect(rendered).to match /Cake/
+    expect(rendered).to match /Toast/
+    assert_select "tr>td", text: 'Cake', count: 1
+    assert_select "tbody>tr", count: 2
   end
 end
