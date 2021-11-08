5b48816b6e930df7918633c0b6b9cb3721277703
Michael Bamford <michael@michaelbamford.com>
2021-11-09 10:38:13 +1100

    Add timeoutable, trackable to Devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise \
+    :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :validatable,
+    :trackable,
+    :timeoutable, timeout_in: 10.seconds
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'logout', to: 'devise/sessions#destroy'
+  end
+
   devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :recipes do
     resources :comments, only: [:index, :new, :create]
   end
 
-  resource :session, only: [:show, :new, :create, :destroy]
-
-  get 'login', to: 'sessions#new'
-  get 'logout', to: 'sessions#destroy'
+  # resource :session, only: [:show, :new, :create, :destroy]
 
   root to: 'recipes#index'
 end
